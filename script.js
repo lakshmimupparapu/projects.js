d59ae8684345c81d8f144fdb6ed9e6f3af5ac0ae
lakshmimupparapu <115992083+lakshmimupparapu@users.noreply.github.com>
2024-04-02 00:14:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let FirstName = document.getElementById('First Name');
+console.log(FirstName.value)
+
+FirstName.addEventLisistener("input",function(){
+    console.log(FirstName.value)
+});
+
+let LastName = document.getElementById('LastName');
+console.log(LastName.value)
+
+LastName.addEventLisistener("input",function(){
+    console.log(LastName.value)
+});
+
+
+let Manager = document.getElementById('Manager');
+console.log(Manager.value)
+
+Manager.addEventLisistener("input",function(){
+    console.log(Manager.value)
+});
+
+
+let ManagerName = document.getElementById('ManagerName');
+console.log(ManagerName.value)
+
+Manager.addEventLisistener("input",function(){
+    console.log(ManagerName.value)
+});
+
+
+let task = document.getElementById('task');
+console.log(task.value)
+
+task.addEventLisistener("input",function(){
+    console.log(task.value)
+});
+
+let task1 = document.getElementById('task1');
+console.log(task1.value)
+
+task1.addEventLisistener("input",function(){
+    console.log(task1.value)
+});
+
+
